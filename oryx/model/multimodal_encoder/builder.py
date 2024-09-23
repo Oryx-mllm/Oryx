432ae84cf57c73e56102706080b9699fba950e9a
dongyh20 <dongyh20@mails.tsinghua.edu.cn>
2024-09-23 16:05:42 +0800

    update model loading

--- oryx/model/multimodal_encoder/builder.py
+++ oryx/model/multimodal_encoder/builder.py
@@ -2,10 +2,10 @@ import os
 from .oryx_vit import OryxViTWrapper
 
 def build_vision_tower(vision_tower_cfg, **kwargs):
-    vision_tower = getattr(vision_tower_cfg, 'mm_vision_tower', getattr(vision_tower_cfg, 'vision_tower', None))
+    vision_tower = getattr(vision_tower_cfg, 'vision_tower', getattr(vision_tower_cfg, 'mm_vision_tower', None))
     is_absolute_path_exists = os.path.exists(vision_tower)
     if "oryx_vit" in vision_tower:
-        print("Buiding OryxViTWrapper...")
+        print(f"Buiding OryxViTWrapper from {vision_tower}...")
         path = vision_tower.split(":")[1]
         return OryxViTWrapper(vision_tower, path=path, args=vision_tower_cfg, **kwargs)
     else:
